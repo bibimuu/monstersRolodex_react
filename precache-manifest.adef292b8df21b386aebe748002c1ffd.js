3c60dd9c455977338671026f0f88c70ad46f2dea
平松大悟朗 <h.dai56@gmail.com>
2020-07-22 13:31:58 +0900

    Updates

---
+++ precache-manifest.adef292b8df21b386aebe748002c1ffd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25dac84c2d4b629449ea60850f95711",
+    "url": "/monstersRolodex_react/index.html"
+  },
+  {
+    "revision": "942f61285d89c5d51dbd",
+    "url": "/monstersRolodex_react/static/css/main.26c8612a.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monstersRolodex_react/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstersRolodex_react/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "942f61285d89c5d51dbd",
+    "url": "/monstersRolodex_react/static/js/main.5cc98b52.chunk.js"
+  },
+  {
+    "revision": "21593ca8e3c2c7da5235",
+    "url": "/monstersRolodex_react/static/js/runtime-main.9413032f.js"
+  }
+]);
